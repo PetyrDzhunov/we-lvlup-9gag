75d4c7bc05ed56278e4db2c397113231fd34b0d8
Petar Dzhunov <petar.djunov@abv.bg>
2022-03-01 13:27:28 +0200

    fix: connect jquery to html

---
+++ src/app.js
@@ -0,0 +1,7 @@
+/* eslint-disable prefer-arrow-callback */
+/* eslint-disable func-names */
+/* eslint-disable no-undef */
+
+$(function () {
+  $('#app').text('Hello').css('color', '#0088ff');
+});
