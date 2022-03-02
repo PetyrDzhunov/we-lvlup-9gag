b61a5d182170ba8a110ffadc4f4530021cd86cfd
Petar Dzhunov <petar.djunov@abv.bg>
2022-03-02 11:14:14 +0200

    feat: found a way to integrate jQuery into the project

--- src/views/homePage.js
+++ src/views/homePage.js
@@ -1,3 +1,4 @@
+/* eslint-disable indent */
 /* eslint-disable import/prefer-default-export */
 /* eslint-disable import/extensions */
 /* eslint-disable func-names */
@@ -5,7 +6,20 @@
 /* eslint-disable prefer-arrow-callback */
 import { html } from '../lib.js';
 
-const homeTemplate = () => html` <section>homePage</section> `;
+function jQueryFunction() {
+  $(function () {
+    // eslint-disable-next-line no-alert
+    $('h1').text('Changed text').css('background-color', 'blue');
+  });
+}
+
+const homeTemplate = () =>
+  html`
+    ${jQueryFunction()}
+    <section>
+      <h1>9GAG</h1>
+    </section>
+  `;
 
 export async function homePage(ctx) {
   ctx.render(homeTemplate());
