0e034d79f5fde916c58c67257ea8a693f5398434
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2022-03-10 12:51:47 +0200

    fix: fix logout functionality to add an event on the button only once not 100 times

--- src/middlewares/decorateContext.js
+++ src/middlewares/decorateContext.js
@@ -1,11 +1,12 @@
-import attachLogout from '../utils/authentication/logout.js';
 import { render } from 'https://unpkg.com/lit-element/lit-element.js?module';
+import logout from '../utils/authentication/logout.js';
+import userData from '../utils/data/userData.js';
 
 const root = document.getElementById('page-content');
 
+
+
 export default function decorateContext(ctx, next) {
   ctx.render = (content) => render(content, root);
-
-  attachLogout();
   next();
 }
--- src/utils/authentication/logout.js
+++ src/utils/authentication/logout.js
@@ -1,11 +1,12 @@
 import userData from '../data/userData.js';
 
-export default function attachLogout() {
+export default (function attachLogout() {
   $(document).ready(function () {
-    $('.logout-btn').click(function () {
+    $('.logout-btn').click(async function () {
       userData.clearUserData();
+      await auth.signOut();
       $('.logout-btn').text('Sign up');
       $('#login-btn').text('Log in');
     });
   });
-}
+})();
