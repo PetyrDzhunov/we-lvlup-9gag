92589c8b12d71972f28517442f269ccbe1f3596b
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2022-03-10 17:03:24 +0200

    fix: created navbar stick to the top while scrolling through gifs

--- src/templates/freshTemplate.js
+++ src/templates/freshTemplate.js
@@ -6,22 +6,12 @@ import { html } from 'https://unpkg.com/lit-element/lit-element.js?module';
 
 const singleGiphy = (giphy) => html`
 ${console.log(giphy)}
-<!-- <div class="giphy-box__outt">
-  <div class="giphy-box__inner">
-    <p class="giphy__name">Name: ${giphy.name}</p>
-    <p class="artist">Artist: ${giphy.artist}</p>
-    <p class="genre">Genre: ${giphy.genre}</p>
-    <p class="price">Price: $${giphy.price}</p>
-    <p class="date">Release Date: ${giphy.releaseDate}</p>
-  </div>
-  <img class="text-center" src=${giphy.images.downsized_large.url}>
-
-</div> -->
 <div class="card m-2" style="width: 40rem;">
-  <img class="card-img-top" src=${giphy.images.downsized_large.url} alt="Card image cap">
   <div class="card-body">
     <h5 class="card-title">${giphy.title}</h5>
   </div>
+  <img class="card-img-top" src=${giphy.images.downsized_large.url} alt="Card image cap">
+
 </div>
 `;
 
