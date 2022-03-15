7a731c13317416b45cbb89fb89850bb74ed4e733
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2022-03-15 10:20:13 +0200

    feat: add signUpCard to the layout

--- src/templates/homeTemplate.js
+++ src/templates/homeTemplate.js
@@ -1,9 +1,5 @@
-import homePageJqueryFunction from '../utils/dom/homejQuery.js';
 import { html } from 'https://unpkg.com/lit-element/lit-element.js?module';
 
 export default function homeTemplate() {
-  return html`
-    <section>homepage</section>
-    ${homePageJqueryFunction()}
-  `;
+  return html` <section>homepage</section> `;
 }
