57ab85fa90b2aac2d8f59455022ea6ce8a8b16c0
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2022-03-15 11:59:52 +0200

    fix: fix the styling of the login/register modal

--- src/templates/components/modal.js
+++ src/templates/components/modal.js
@@ -46,12 +46,16 @@ export default function modalTemplate(
                 <span aria-hidden="true">&times;</span>
               </button>
             </div>
-            <button type="button" class="btn btn-primary m-2" id="fb-btn">
-              Login with Facebook
-            </button>
-            <button type="button" class="btn btn-primary m-2" id="google-btn">
-              Login with Google
-            </button>
+            <div class="modal__social-button__list">
+              <button type="button" class="modal__btn" id="fb-btn">
+                <i class="bi bi-facebook"></i>
+                Countinue with Facebook
+              </button>
+              <button type="button" class="modal__btn" id="google-btn">
+                <i class="bi bi-google"></i>
+                Countinue with Google
+              </button>
+            </div>
             <div class="modal-body d-flex flex-column">
               ${requiresFullName
                 ? html`<input
@@ -64,31 +68,23 @@ export default function modalTemplate(
                 : null}
               <input
                 type="email"
-                class="m-2"
+                class="m-2 modal-input "
                 id="email"
                 placeholder="Email adress"
                 name="email"
               />
               <input
-                class="m-2"
+                class="m-2 modal-input "
                 id="password"
                 type="password"
                 placeholder="Password"
                 name="password"
               />
             </div>
-            <div class="modal-footer">
-              <button
-                type="button"
-                class="btn btn-secondary"
-                data-bs-dismiss="modal"
-              >
-                Close
-              </button>
-              <button type="submit" class="btn btn-primary">
-                ${authenticate}
-              </button>
-            </div>
+            <button type="submit" class="btn btn-primary modal__submit-btn">
+              ${authenticate}
+            </button>
+            <p class="modal__text">Forgot Password?</p>
           </div>
         </div>
       </div>
