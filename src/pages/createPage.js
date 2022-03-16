5f228116a720ecdde73663be3c9642618e15c02a
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2022-03-16 16:57:49 +0200

    feat: creating the onUpload function to upload gif/meme/img

--- src/pages/createPage.js
+++ src/pages/createPage.js
@@ -1,8 +1,33 @@
 import createMemeTemplate from '../templates/createTemplate.js';
+// import request from '../utils/fetch/fetch.js';
+import { giphyAPIkey } from '../constants.js';
 
 export default async function createPage(ctx) {
+  function onUploadSubmit(e) {
+    e.preventDefault();
+    const currentFormClicked = e.target.id;
+    const imagePreview = document.getElementById('image-preview');
 
-  async function onUploadSubmit()
+    if (currentFormClicked === 'file-upload') {
+      $.ajax({
+        url: 'http://upload.giphy.com/v1/gifs?',
+        type: 'POST',
+        dataType: '',
+        data: {
+          q: 'keyword',
+          api_key: giphyAPIkey,
+          source_image_url: imagePreview.src,
+        },
+        success: (data) => {
+          console.log('success');
+          console.log(data);
+        },
+        error: (err) => {
+          console.log(err);
+        },
+      });
+    }
+  }
 
   ctx.render(createMemeTemplate(onUploadSubmit));
 }
--- src/templates/createTemplate.js
+++ src/templates/createTemplate.js
@@ -16,18 +16,18 @@ export default function createMemeTemplate(onUploadSubmit) {
   return html`
     <h1>Create new GIF or Meme</h1>
     <h5 class="mb-5">You can upload from your file system or you can send a URL</h5 class="mb-5">
-    <form @submit="${onUploadSubmit}" class="input-group flex-nowrap mb-3">
+    <form @submit="${onUploadSubmit}" class="input-group flex-nowrap mb-3" id="file-upload">
       <div class="input-group mb-3" id="inputGroup-sizing-default">
-        <input type="file" @change="${loadFile}" class="form-control" id="inputGroupFile02" />
+        <input type="file" @change="${loadFile}" class="form-control" id="inputGroupFile02" name="file-input" />
         <button class="btn btn-primary" type="submit">Upload</button>
       </div>
     </form>
     
-    <form @submit="${onUploadSubmit}" class="input-group flex-nowrap mb-3">
+    <form @submit="${onUploadSubmit}" class="input-group flex-nowrap mb-3" id="url-upload">
       <label for="basic-url" class="form-label">Your giphy URL</label>
         <div class="input-group mb-3">
           <label class="input-group-text" id="basic-addon3">https://giphy.com/gifs/</label>
-          <input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3">
+          <input type="text" @change="${loadFile}" name="url-input" class="form-control" id="basic-url" aria-describedby="basic-addon3">
           <button class="btn btn-primary" type="submit">Upload</button>
         </div>
     </form>
