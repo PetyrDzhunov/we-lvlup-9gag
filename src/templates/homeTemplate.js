7f3b2520ba0490f77356b75a88b52014d0ca11cd
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2022-03-22 10:38:22 +0200

    fix: create a partial to improve code reusability

--- src/templates/homeTemplate.js
+++ src/templates/homeTemplate.js
@@ -1,17 +1,5 @@
+import singleGiphy from './partials/singleGiphy.js';
 import { html } from 'https://unpkg.com/lit-element/lit-element.js?module';
-
-const singleGiphy = (giphy) => html`
-  <div class="card m-2" style="width: 40rem;">
-    <div class="card-body">
-      <h5 class="card-title">${giphy.title}</h5>
-    </div>
-    <img
-      class="card-img-top"
-      src=${giphy.images.downsized_large.url}
-      alt="Card image cap"
-    />
-  </div>
-`;
 
 export default function homeTemplate(mostLikedGiphies) {
   return html` ${mostLikedGiphies.map(singleGiphy)}`;
---
+++ src/templates/partials/singleGiphy.js
@@ -0,0 +1,14 @@
+export default function singleGiphy(giphy) {
+  return html`
+    <div class="card m-2" style="width: 40rem;">
+      <div class="card-body">
+        <h5 class="card-title">${giphy.title}</h5>
+      </div>
+      <img
+        class="card-img-top"
+        src=${giphy.images.downsized_large.url}
+        alt="Card image cap"
+      />
+    </div>
+  `;
+}
