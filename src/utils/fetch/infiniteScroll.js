2bab77154f9dbadd019dbdc9c460fc6edbc2a1b8
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2022-03-22 17:24:30 +0200

    fix: small infinite scroll fix

--- src/utils/fetch/infiniteScroll.js
+++ src/utils/fetch/infiniteScroll.js
@@ -1,77 +1,70 @@
 import { giphyAPIkey } from '../../constants.js';
 import { giphyApiEndpoints } from '../../giphyEndpoints.js';
 
-if (
-  window.location.pathname === '/' ||
-  window.location.pathname === '/fresh-memes'
-) {
-  $(document).ready(() => {
-    let offset = 0;
-    // initial value for offset
-    let offsetVal = 0;
-    // set your limit
-    const giphyLimit = 10;
+let offset = 0;
+// initial value for offset
+let offsetVal = 0;
+// set your limit
+const giphyLimit = 10;
 
-    function getGiphy(i) {
-      // if offset is greater than one then fetch further items prior to previous ones
-      if (i > 0) {
-        // increase the offset with item limit like 25, 50 to get the next items
-        offsetVal = giphyLimit * i;
-      }
-      $.ajax({
-        url: giphyApiEndpoints.trending,
-        type: 'GET',
-        dataType: 'json',
-        data: {
-          q: 'keyword',
-          api_key: giphyAPIkey,
-          limit: giphyLimit,
-          offset: offsetVal,
-        },
-        success: (data) => {
-          console.log(offset);
-          console.log(data.pagination);
-          $.each(data.data, (index, giphy) => {
-            const imageUrl = giphy.images.downsized_large.url;
-            $('#page').append(`
+function getGiphy(i) {
+  // if offset is greater than one then fetch further items prior to previous ones
+  if (i > 0) {
+    // increase the offset with item limit like 25, 50 to get the next items
+    offsetVal = giphyLimit * i;
+  }
+  $.ajax({
+    url: giphyApiEndpoints.trending,
+    type: 'GET',
+    dataType: 'json',
+    data: {
+      q: 'keyword',
+      api_key: giphyAPIkey,
+      limit: giphyLimit,
+      offset: offsetVal,
+    },
+    success: (data) => {
+      console.log(offset);
+      console.log(data.pagination);
+      $.each(data.data, (index, giphy) => {
+        const imageUrl = giphy.images.downsized_large.url;
+        $('#page-content').append(`
          <div class="card m-2" style="width: 40rem;">
             <div class="card-body">
                 <h5 class="card-title">${giphy.title}</h5>
              </div>
            <img class="card-img-top" src=${imageUrl} alt="Giphy image"/>
          </div>`);
-          });
-          // increase offset to get further items.
-          offset += 1;
-        },
-        error: (err) => {
-          console.log(err);
-        },
       });
-    }
-    getGiphy(0);
-
-    let paginate = true;
-
-    const paginateToggle = function () {
-      paginate = true;
-    };
-
-    $(window).scroll(function () {
-      // when scroll reaches to bottom.
-      if (
-        $(window).scrollTop() >=
-        $(document).height() - $(window).height() - 1500
-      ) {
-        if (paginate) {
-          paginate = false;
-          console.log('pagination');
-          getGiphy(offset);
-          setTimeout(paginateToggle, 1000);
-        } else {
-          console.log('only one pagination allowed per scroll down');
-        }
-      }
-    });
+      // increase offset to get further items.
+      offset += 1;
+    },
+    error: (err) => {
+      console.log(err);
+    },
   });
 }
+getGiphy(0);
+
+let paginate = true;
+
+const paginateToggle = function () {
+  paginate = true;
+};
+
+$(window).scroll(function () {
+  // when scroll reaches to bottom.
+  if (
+    $(window).scrollTop() >=
+    $(document).height() - $(window).height() - 1500
+  ) {
+    if (paginate) {
+      paginate = false;
+      console.log('pagination');
+      getGiphy(offset);
+      setTimeout(paginateToggle, 1000);
+    } else {
+      console.log('only one pagination allowed per scroll down');
+    }
+  }
+});
