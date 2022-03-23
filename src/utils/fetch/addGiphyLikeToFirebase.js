e536858e24736098c6eafbf2e9435adaf9802a64
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2022-03-23 15:06:29 +0200

    feat: created like functionality

---
+++ src/utils/fetch/addGiphyLikeToFirebase.js
@@ -0,0 +1,30 @@
+import { gifsRef } from '../../constants.js';
+
+export default async function addGiphyLikeToFirebase(userId, gifId) {
+  try {
+    const existingGifs = await gifsRef.get();
+    let isAlreadyInDatabase = false;
+    let currentGiphy;
+    existingGifs.forEach((doc) => {
+      const currentGiphyId = doc.data().gif;
+      const currentDocId = doc.id;
+      if (currentGiphyId === gifId) {
+        isAlreadyInDatabase = true;
+        currentGiphy = currentDocId;
+      }
+    });
+
+    if (isAlreadyInDatabase) {
+      return gifsRef.doc(currentGiphy).update({
+        likes: firebase.firestore.FieldValue.arrayUnion(userId),
+      });
+    }
+
+    return gifsRef.doc(userId).set({
+      gif: gifId,
+      likes: [userId],
+    });
+  } catch (error) {
+    return error;
+  }
+}
--- src/utils/fetch/likeGiphy.js
+++ src/utils/fetch/likeGiphy.js
@@ -1,4 +1,4 @@
-import getSingleGiphy from './getSingleGiphyById.js';
+import addGiphyLikeToFirebase from './addGiphyLikeToFirebase.js';
 import userData from '../data/userData.js';
 
 export default async function likeGiphy(e) {
@@ -10,10 +10,10 @@ export default async function likeGiphy(e) {
   }
 
   const currentGiphyId = e.target.parentElement.parentElement.dataset.id;
-  const currentGiphy = await getSingleGiphy(currentGiphyId);
-  console.log(currentGiphy);
 
-  //add this giphy into firebase with an ID and attach to it likes as an array and put the current user id in there
+  // add this giphy into firebase with an ID and attach to it likes as an array and put the current user id in there
+  await addGiphyLikeToFirebase(user.uid, currentGiphyId);
 
+  isLiked = true;
   // after that I can use the length of the array to show how many likes this giphy has
 }
