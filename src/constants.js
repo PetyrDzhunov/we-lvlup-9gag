491bd8f345eb22e913741bc8bf6ba4713a82b7c0
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2022-04-06 17:26:19 +0300

    fix: change giphy api_key and minor css fix on comments section in dark mode

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
 export const PORT = '3000';
 export const baseUrl = 'http://localhost:3000';
-export const giphyAPIkey = 'RI168EIQdPFZaqXKfjBtDCY23um43GGs';
+export const giphyAPIkey = '89hUy2L6QI4iV5Svdp6ZGJEJR3inKFcv';
 export const gifsRef = db.collection('gifs');
